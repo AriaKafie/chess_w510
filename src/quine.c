1653525bc59ed3a680a1d4a808f028b3ab636783
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-09-22 00:16:38 -0700

    Update quine.c

--- src/quine.c
+++ src/quine.c
@@ -1,16 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define path "child.c"
-#define write "w"
-#define compile_and_run "gcc child.c && ./a.out"
  
-const char* c = "#include <stdlib.h>%c#include <stdio.h>%c#define path %cchild.c%c%c#define write %cw%c%c#define compile_and_run %cgcc child.c && ./a.out%c%cconst char* c = %c%s%c;int main() { FILE* child = fopen(path, write);fprintf(child, c, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 34, c, 34);}";
+char c[] = "#include <stdlib.h>%c#include <stdio.h>%cchar c[] = %c%s%c;%c%cint main(int argc, char** argv) { char command_string[50] = %cgcc %c; char ver_string[12]; int ver_num = atoi(argv[1])+1; sprintf(ver_string, %c%cd%c, ver_num); char child_name[18] = %cchild%c; char dot_c_tack_o[] = %c.c -o %c; char and_dot_slash[] = %c && ./%c; strcat(child_name, ver_string); strcat(command_string, child_name); strcat(command_string, dot_c_tack_o); strcat(command_string, child_name); strcat(command_string, and_dot_slash); strcat(command_string, child_name); char space[] = %c %c; strcat(command_string, space); strcat(command_string, ver_string); char dot_c[] = %c.c%c; strcat(child_name, dot_c); FILE* child = fopen(child_name, %cw%c); fprintf(child, c, 10, 10, 34, c, 34, 10, 10, 34, 34, 34, 37, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34); system(command_string);}";
  
-int main() {
+int main(int argc, char** argv)
+{
+  char command_string[50] = "gcc ";
+  char ver_string[12];
+  int ver_num = atoi(argv[1]) + 1;
+  sprintf(ver_string, "%d", ver_num);
  
-    FILE* child = fopen(path, write);
+   char child_name[18] = "child";
+   char dot_c_tack_o[] = ".c -o ";
+   char and_dot_slash[] = " && ./"; 
+   strcat(child_name, ver_string); 
  
-    fprintf(child, c, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 34, c, 34);
+   strcat(command_string, child_name); 
+   strcat(command_string, dot_c_tack_o); 
+   strcat(command_string, child_name); 
+   strcat(command_string, and_dot_slash); 
+   strcat(command_string, child_name); 
  
-    system(compile_and_run);
+   char space[] = " "; 
+ 
+   strcat(command_string, space);
+   strcat(command_string, ver_string); 
+ 
+   char dot_c[] = ".c";
+ 
+   strcat(child_name, dot_c);
+ 
+   FILE* child = fopen(child_name, "w");
+ 
+   fprintf(child, c, 10, 10, 34, c, 34, 10, 10, 34, 34, 34, 37, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34);
+ 
+   system(command_string);
 }
