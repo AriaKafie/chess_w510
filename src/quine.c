a53a2d10b231a8aec599ece5339f6c50df0ff1d8
9199126091_1421 <135975396+AriaKafie@users.noreply.github.com>
2024-09-22 17:04:41 -0700

    Update quine.c

--- src/quine.c
+++ src/quine.c
@@ -2,25 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
  
-char c[] = "#include <stdlib.h>%c#include <stdio.h>%cchar c[] = %c%s%c;%c%cint main(int argc, char** argv) { char command_string[50]; char ver_string[12]; int ver_num = atoi(argv[1]) + 1; sprintf(ver_string, %c%cd%c, ver_num); char child_name[18] = %cchild%c; strcat(child_name, ver_string); snprintf(command_string, sizeof(command_string), %cgcc %cs.c -o%cs; ./%cs %cs%c, child_name, child_name, child_name, ver_string); char path[20]; snprintf(path, sizeof(path), %c%cs.c%c, child_name); FILE* child = fopen(path, %cw%c); fprintf(child, c, 10, 10, 34, c, 34, 10, 10, 34, 37, 34, 34, 34, 34, 37, 37, 37, 37, 34, 34, 37, 34, 34, 34); fclose(child); system(command_string); }";
+char c[] = "#include <stdlib.h>%c#include <stdio.h>%cchar c[] = %c%s%c;int main(int argc, char** argv) { int version = atoi(argv[1]) + 1; char path[20]; snprintf(path, sizeof(path), %cchild%cd.c%c, version); char command_string[100]; snprintf(command_string, sizeof(command_string), %cgcc %cs -ochild%cd; ./child%cd %cd%c, path, version, version, version); FILE* child = fopen(path, %cw%c); fprintf(child, c, 10, 10, 34, c, 34, 34, 37, 34, 34, 37, 37, 37, 37, 34, 34, 34); fclose(child); system(command_string); }";
  
 int main(int argc, char** argv)
 {
-   char command_string[50];
-   char ver_string[12];
-   int ver_num = atoi(argv[1]) + 1;
-   sprintf(ver_string, "%d", ver_num);
+   int version = atoi(argv[1]) + 1;
  
-   char child_name[18] = "child";
-   strcat(child_name, ver_string);
- 
-   snprintf(command_string, sizeof(command_string), "gcc %s.c -o%s; ./%s %s", child_name, child_name, child_name, ver_string);
    char path[20];
-   snprintf(path, sizeof(path), "%s.c", child_name);
+   snprintf(path, sizeof(path), "child%d.c", version);
+ 
+   char command_string[100];
+   snprintf(command_string, sizeof(command_string), "gcc %s -ochild%d; ./child%d %d", path, version, version, version);
  
    FILE* child = fopen(path, "w");
  
-   fprintf(child, c, 10, 10, 34, c, 34, 10, 10, 34, 37, 34, 34, 34, 34, 37, 37, 37, 37, 34, 34, 37, 34, 34, 34);
+   fprintf(child, c, 10, 10, 34, c, 34, 34, 37, 34, 34, 37, 37, 37, 37, 34, 34, 34);
    fclose(child);
  
    system(command_string);
